64612ea994a0c2da8a6e9cc294a7737b99f7ba81
Jared Cacurak <jared.cacurak@gmail.com>
2013-07-09 00:37:39 -0400

    Add documentation.

--- puzzle.js
+++ puzzle.js
@@ -1,18 +1,36 @@
 (function (define) { 'use strict';
 define(function (require) {
+
+	// Public API
 	return {
 		build: build,
 		reserve: reserve
 	};
 
+	/**
+	 * Simply builds and returns a data structure to represent a seating map.
+	 *
+	 * @param {number} rows The number of rows.
+	 * @param {number} cols The number of columns (a.k.a. seats per row)
+	 * @param {array} reserved A 1D array of strings representing the seats that are already reserved (formatted as "R1C1")
+	 * @returns {array}
+	 */
 	function build(rows, cols, reserved) {
-		return toVenue(without(reserved, buildBlocks(rows, cols)))
+		return seatingMap(without(reserved, buildBlocks(rows, cols)))
 				.sort(byAscending('distance'));
 	}
 
+	/**
+	 * Reserves the best available contiguous block of n seats and returns map with those seats reserved.
+	 *
+	 * @param {array} map The seating map.
+	 * @param {number} n The number of contiguous seats in a row someone is looking to reserve.
+	 * @returns {array} The map with the best available seats now reserved.
+	 */
 	function reserve(map, n) {
 		var i, seats, found, reserved;
 
+		// guard against reservation requests greater than ten
 		if (n > 10) {
 			console.log('The maximum tickets that can request at once is 10');
 			return map;
@@ -21,27 +39,41 @@ define(function (require) {
 		for (i = 0; i <= map.length; i += 1) {
 			seats = map[i] && map[i].seats;
 
+			// if an acceptable seating was found
 			if (seats && seats.length >= n) {
 				found = true;
 
+				// if the number of seats is an exact match
+				// delete the element from the map
 				if (seats.length === n) {
 					reserved = seats;
 					delete map[i];
-				} else {
+				}
+				// else reserve the best availabe seats by removing them
+				// then recalculate the new seating blocks distance with
+				// regards to the map
+				else {
 					reserved = seats.splice(0, n);
 					map[i].distance = seats[0].distance;
 					map.sort(byAscending('distance'));
 				}
-				print(reserved);
+				print(reserved);	// print the start and end seats that were just reserved
 				break;
 			}
 		}
 
+		// if it's not possible to fulfill a request
 		if (!found) { console.log('Not Available'); }
 		return map;
 	}
 
-	function toVenue(blocks) {
+	/**
+	 * Creates a data structure that represents the seating map.
+	 *
+	 * @param {array} blocks The seating blocks to be evaluated.
+	 * @returns {array} An array of objects containing a seating block and the least distance to front and center.
+	 */
+	function seatingMap(blocks) {
 		return blocks.filter(function (block) {
 			return block.length;
 		}).map(function (block) {
@@ -53,13 +85,28 @@ define(function (require) {
 		});
 	}
 
+	/**
+	 * A partial function used to make sorting more palatable.
+	 *
+	 * @param {string} property The property to examine.
+	 * @returns {function} A function that calculates the difference of two properties.
+	 */
 	function byAscending(property) {
 		return function (a, b) {
 			return a[property] - b[property];
 		};
 	}
 
+	/**
+	 * Removes reserved seats from the seating blocks.
+	 *
+	 * @param reserved {array} A 1D array of strings representing the seats that are already reserved (formatted as "R1C1")
+	 * @param blocks {array} The seating blocks.
+	 * @returns {array} The seating blocks with the reserved seats removed.
+	 */
 	function without(reserved, blocks) {
+
+		// guard against an empty array of reserved seats
 		if (!reserved.length) { return blocks; }
 
 		return blocks.reduce(function (memo, block) {
@@ -71,12 +118,14 @@ define(function (require) {
 			reserved.forEach(function (reservedSeat, i) {
 				var seat = values.indexOf(reservedSeat, from);
 
-				if (seat >= 0) {
+				// capture the the seats leading upto the reserved seat
+				if (seat >= 0) {	// the reserved seat was found
 					memo.push(block.slice(from, seat));
 					from = seat + 1;
 				}
 
-				if (reserved.length === i + 1) {
+				// capture the rest of the seats in the row
+				if (reserved.length === i + 1) {	// exhausted the reserved seats
 					memo.push(block.slice(from - block.length));
 				}
 			});
@@ -85,6 +134,13 @@ define(function (require) {
 		}, []);
 	}
 
+	/**
+	 * Build an array of seating blocks.
+	 *
+	 * @param row {number} The number of rows.
+	 * param cols {number} The number of columns (a.k.a. seats per row).
+	 * returns {array} The seating blocks.
+	 */
 	function buildBlocks(rows, cols) {
 		var blocks, bestSeat, r, block, c;
 
@@ -106,16 +162,32 @@ define(function (require) {
 		return blocks;
 	}
 
+	/**
+	 * Print the start and end seats.
+	 *
+	 * @param {array} seats The seats to be printed.
+	 * @returns {array} The seats passed in.
+	 */
 	function print(seats) {
+
+		// if there is only one seat print that seat's value
 		if (seats.length === 1) {
 			console.log(seats[0].value);
-		} else {
+		}
+		// else print the outter most seat values
+		else {
 			seats.sort(byAscending('column'));
 			console.log(seats[0].value + '-' + seats[seats.length - 1].value);
 		}
 		return seats;
 	}
 
+	/**
+	 * Calculates the column number of the front center seat(s).
+	 * 
+	 * @param {number} cols The number of columns.
+	 * @returns {array} The center seat(s).
+	 */
 	function frontAndCenter(cols) {
 		var center, parity;
 
@@ -124,6 +196,15 @@ define(function (require) {
 		return (parity) ? [center] : [center, center + 1];
 	}
 
+	/**
+	 * Calculates the Manhattan Distance between a seat in the front row and any other seat.
+	 *
+	 * @see <a href="http://en.wikipedia.org/wiki/Taxicab_geometry">Taxicab geometry</a>
+	 * @param {number} from The column in the front row.
+	 * @param {number} row The row of the seat.
+	 * @param {number} column The column of the seat.
+	 * @returns {number} The closest distance.
+	 */
 	function manhattanDistance(from, row, column) {
 		return from.map(function (seat) {
 			return Math.abs(1 - row) + Math.abs(seat - column);
